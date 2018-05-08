5b98198d0ab1a83affdb945dbb0d4b924ac4e339
SandraB <slbkc7@mail.missouri.edu>
2018-05-08 06:24:53 -0500

    game of tic tac toe

---
+++ TicTacToe/TicTacToe/TicTacToeViewController.swift
@@ -0,0 +1,120 @@
+//
+//  TicTacToeViewController.swift
+//  TicTacToe
+//
+//  Created by Sandra Berndt on 5/8/18.
+//  Copyright © 2018 Sandra Berndt. All rights reserved.
+//
+
+import UIKit
+
+class TicTacToeViewController: UIViewController {
+
+    var activePlayer = 1 //cross
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    
+    let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    var gameIsActive = true
+    
+    
+    
+    @IBOutlet weak var winnerLabel: UILabel!
+    @IBAction func ticTacAction(_ sender: AnyObject)
+    {
+        if (gameState[sender.tag-1] == 0 && gameIsActive == true)
+        {
+            gameState[sender.tag-1] = activePlayer
+            if (activePlayer == 1)
+            {
+                sender.setImage(UIImage(named: "Cross.png"), for: UIControlState())
+                activePlayer = 2
+            }
+            else
+            {
+                sender.setImage(UIImage(named: "Nought.png"), for: UIControlState())
+                activePlayer = 1
+            }
+        }
+        
+        for combination in winningCombinations
+        {
+            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]
+            {
+                gameIsActive = false
+                
+                if gameState[combination[0]] == 1
+                {
+                    //cross has won
+                    label.txt = "Cross has WON!"
+                    
+                }
+                else
+                {
+                    //nought has won
+                    label.txt = "Circle has WON!"
+                }
+                
+                playAgainButton.isHidden = false
+                label.isHidden = false
+            }
+        }
+        
+        gameIsActive = false
+        
+        for i in gameState
+        {
+            if i == 0
+            {
+                gameIsActive = true
+                break
+            }
+        }
+        
+        if gameIsActive == false
+        {
+            label.text = "It was a Draw!"
+            label.isHidden = false
+            playAgainButton.isHidden = false
+        }
+    }
+    
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBAction func playAgain(_ sender: AnyObject)
+    {
+        gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        gameIsActive = true
+        activePlayer = 1
+        
+        playAgainButton.isHidden = true
+        UILabel.isHidden = true
+        
+        for i in 1...9
+        {
+            let button = view.viewWithTag(i) as! UIButton
+            button.setImage(nil, for: UIControlState())
+        }
+    }
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
